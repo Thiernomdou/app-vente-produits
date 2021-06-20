dbdba79943b602942dcbd4ddb7a2a2c1201fa3a2
DIALLO Thierno Mamadou <thiernomdou63@gmail.com>
2021-06-20 23:57:51 +0200

    products

--- Projets/VueJS/vente-produit/src/store/product/actions.js
+++ Projets/VueJS/vente-produit/src/store/product/actions.js
@@ -2,4 +2,16 @@
  avec Axios, une fonction pour récupérer les détails d'un produit, 
  une fonction pour ajouter le produit au panier et 
  une fonction pour retirer le produit du panier*/
- 
+import axios from "axios"
+import { get } from "core-js/core/dict";
+
+//Action to get products list
+
+    export function getProducts({ commit }) {
+        let url = "https://my-json-server.typicode.com/Nelzio/ecommerce-fake-json/products";
+        axios.get(url).then((response) => {
+            commit("setProducts", response.data);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
